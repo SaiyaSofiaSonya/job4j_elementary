6d7a4cabd464017493f67a9a050839757fdb10a7
SaiyaSofiaSonya <57871707+SaiyaSofiaSonya@users.noreply.github.com>
2021-07-13 09:16:38 +0200

    4.1.2. Глупый бот [#31323]

---
+++ test/ru/job4j/condition/DummyBotTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class DummyBotTest  {
+
+    @Test
+    public void whenGreetBot() {
+        String expected = "Привет, умник.";
+        String in = "Привет, Бот.";
+        String out = DummyBot.answer(in);
+    }
+
+    @Test
+    public void whenByeBot() {
+        String expected = "До скорой встречи.";
+        String in = "Пока.";
+        String out = DummyBot.answer(in);
+    }
+
+    @Test
+    public void whenUnknownBot() {
+        String expected = "Это ставит меня в тупик. Задайте другой вопрос.";
+        String in = "Привет";
+        String out = DummyBot.answer(in);
+    }
+
+}
