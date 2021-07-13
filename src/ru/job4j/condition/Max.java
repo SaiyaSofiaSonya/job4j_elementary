9d437bd34d1c3204da4f74d973daa5cf3f1570ba
SaiyaSofiaSonya <57871707+SaiyaSofiaSonya@users.noreply.github.com>
2021-07-13 20:13:30 +0200

    4.2. Тернарное сравнение [#189 #177298]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -3,7 +3,6 @@ package ru.job4j.condition;
 public class Max {
 
     public static int max(int left, int right) {
-        boolean leftMoreTheRightOne = left > right;
-        return leftMoreTheRightOne ? left : right;
+        return left > right ? left : right;
     }
 }
